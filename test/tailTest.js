8af657133b7999e068ff0136f5a17c287a49f640
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-04-11 17:55:03 -0400

    test codes are removed and mocha and chai testing is employed

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,16 @@
-const tail = require('../tail')
-const assertEqual = require('../assertEqual')
+const tail = require('../tail');
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns true", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+  });
+  
+});
 
 
 
 
-assertEqual(tail([1,2,3]),[2,3])
+
+
+//assertEqual(tail([1,2,3]),[2,3])
