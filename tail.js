b8d8a26d6a664dc6dd502d0231fa47ee55db9a93
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-04-11 16:00:47 -0400

    tail function modified

--- tail.js
+++ tail.js
@@ -1,17 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("Assertion Passed🧡:" + actual + "===" + expected);
-  }
-  if (actual !== expected) {
-    console.assert('Assertion Failed😈:' + actual + '!==' + expected);
-  }
-};
 
 const tail = function(array) {
-  let result = [];
-  return result.push(array.slice(1));
+  
+  return array.slice(1);
 };
-//Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length,3);
+
+
+
+module.exports = tail;
