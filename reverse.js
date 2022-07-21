66bfd1dd262bb309818e925333ee90d12665ad10
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-21 18:24:39 -0400

    added reverse.js

---
+++ reverse.js
@@ -0,0 +1,20 @@
+let words = process.argv.slice(2)
+//without .reverse method
+function reverseWords(arrOfWords) {
+  let result = []
+  for (let j = 0; j <= arrOfWords.length - 1; j++) {
+    let reversed = ""
+    for (let i = arrOfWords[j].length - 1; i >= 0; i--) {
+      reversed += arrOfWords[j][i]
+    }
+    result.push(reversed)
+  }
+  return result
+}
+//with .reverse method
+function reverseWords(arrOfWords){
+  console.log(arrOfWords.map(word=>{
+    return e.split('').reverse().join('')
+  }))
+}
+console.log(reverseWords(words))
