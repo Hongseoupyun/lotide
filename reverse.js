5daab60aaa9a7b2e7f0ddd3e78426795d5930926
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-21 20:42:27 -0400

    added an another way to reverse.js

--- reverse.js
+++ reverse.js
@@ -17,4 +17,19 @@ function reverseWords(arrOfWords){
     return word.split('').reverse().join('')
   }))
 }
+
+//with charAt
+
+function reverseWords(arrOfWords) {
+  let result = []
+  for (let j = 0; j <= arrOfWords.length - 1; j++) {
+    let reversed = ""
+    for (let i = arrOfWords[j].length - 1; i >= 0; i--) {
+      reversed += arrOfWords[j].charAt(i)
+    }
+    result.push(reversed)
+  }
+  return result
+}
+
 console.log(reverseWords(words))
